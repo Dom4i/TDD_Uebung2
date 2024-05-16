a504ea7584978ca100586be993de0ae75d4c9dd1
=Dominik Schrenk <dsschrenk@gmail.com>
2024-05-16 13:45:03 +0200

    Added 2. Test Marcel

--- string_calculator/spec/string_calculator_spec.js
+++ string_calculator/spec/string_calculator_spec.js
@@ -6,4 +6,8 @@ describe('String Calculator', function() {
 	it('should return "You need to input 4-6 numbers" for an empty array', function() {
 		expect(calculator.add([])).toEqual('You need to input 4-6 numbers');
 	});
+
+	it('should return "You need to input 4-6 numbers" for an array < 4', function() {
+		expect(calculator.add([3, 5, 6])).toEqual('You need to input 4-6 numbers');
+	});
 });
