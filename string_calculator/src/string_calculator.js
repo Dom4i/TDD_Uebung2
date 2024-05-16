e0c21bc8a29163a5f360dc97dde88ec201fa0019
=Dominik Schrenk <dsschrenk@gmail.com>
2024-05-16 13:43:37 +0200

    1. Test green Dominik

--- string_calculator/src/string_calculator.js
+++ string_calculator/src/string_calculator.js
@@ -2,4 +2,9 @@
 function StringCalculator() {}
 StringCalculator.prototype.add = function(string_numbers) {
 	//
+	console.log(string_numbers);
+
+	if (string_numbers.length === 0) {
+		return 'You need to input 4-6 numbers';
+	}
 };
