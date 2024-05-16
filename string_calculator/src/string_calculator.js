69b869e408f11a0028542591adf142aad38333da
=Dominik Schrenk <dsschrenk@gmail.com>
2024-05-16 13:56:41 +0200

    refactored Dominik

--- string_calculator/src/string_calculator.js
+++ string_calculator/src/string_calculator.js
@@ -5,19 +5,10 @@ StringCalculator.prototype.add = function(string_numbers) {
 	console.log(string_numbers);
 	var result = 0;
 
-	if (string_numbers.length === 0) {
-		return 'You need to input 4-6 numbers';
-	}
-	else if (string_numbers.length < 4) {
-		return 'You need to input 4-6 numbers';
-	}
-	else if (string_numbers.length > 6) {
+	if (string_numbers.length < 4 || string_numbers.length > 6) {
 		return 'You need to input 4-6 numbers';
 	} else {
-		return 0;
+		return result;
 	}
-	
-	
-
 	
 };
