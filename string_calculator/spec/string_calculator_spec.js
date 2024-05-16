8f327c40dbd38453614c4403a760b82e5ffd639c
=Dominik Schrenk <dsschrenk@gmail.com>
2024-05-16 13:46:49 +0200

    Added 3. test Dominik

--- string_calculator/spec/string_calculator_spec.js
+++ string_calculator/spec/string_calculator_spec.js
@@ -10,4 +10,8 @@ describe('String Calculator', function() {
 	it('should return "You need to input 4-6 numbers" for an array < 4', function() {
 		expect(calculator.add([3, 5, 6])).toEqual('You need to input 4-6 numbers');
 	});
+
+	it('should return "You need to input 4-6 numbers" for an array > 6', function() {
+		expect(calculator.add([1, 2, 3, 4, 5, 6, 7])).toEqual('You need to input 4-6 numbers');
+	});
 });
