150a3f9154ab10a03c448cfab5de0228dc360cf4
=Dominik Schrenk <dsschrenk@gmail.com>
2024-05-16 13:45:30 +0200

    2. Test green Marcel

--- string_calculator/src/string_calculator.js
+++ string_calculator/src/string_calculator.js
@@ -7,4 +7,7 @@ StringCalculator.prototype.add = function(string_numbers) {
 	if (string_numbers.length === 0) {
 		return 'You need to input 4-6 numbers';
 	}
+	if (string_numbers.length < 4) {
+		return 'You need to input 4-6 numbers';
+	}
 };
